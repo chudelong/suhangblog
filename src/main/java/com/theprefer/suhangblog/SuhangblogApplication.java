87e5053fcd1dcf04a7baf8bea087942289eea4b3
chudelong <1114553919@qq.com>
2018-12-15 20:26:36 +0800

    Initial commit

---
+++ src/main/java/com/theprefer/suhangblog/SuhangblogApplication.java
@@ -0,0 +1,14 @@
+package com.theprefer.suhangblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhangblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhangblogApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/theprefer/suhangblog/SuhangblogApplicationTests.java
@@ -0,0 +1,17 @@
+package com.theprefer.suhangblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuhangblogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
